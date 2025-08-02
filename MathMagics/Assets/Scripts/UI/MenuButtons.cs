c46c190c1c7c03f2d496ca771708f91c3e4a7d6d
qstokes <quincy.f.stokes@gmail.com>
2025-08-02 13:29:44 -0700

    (Update) Logic for when video ends.

--- MathMagics/Assets/Scripts/UI/MenuButtons.cs
+++ MathMagics/Assets/Scripts/UI/MenuButtons.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,10 +8,26 @@ public class MenuButtons : MonoBehaviour
 {
     public GameObject mainMenuUI;
     public VideoPlayer videoPlayer;
+    private void Awake()
+    {
+        videoPlayer.loopPointReached += HandleVideoFinished;
+    }
+
+    private void HandleVideoFinished(VideoPlayer source)
+    {
+        source.Stop();
+        source.gameObject.SetActive(false);
+        print("Video Done.");
+    }
 
     public void PlayButton()
     {
         mainMenuUI.SetActive(false);
         videoPlayer.Play();
+    }
+
+    private void HandleVideoFinished()
+    {
+
     }
 }
