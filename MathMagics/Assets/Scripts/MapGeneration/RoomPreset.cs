3a7577c4ea5322030dcf18e5f6fdf708425a6781
QuincyStokes <97055018+QuincyStokes@users.noreply.github.com>
2025-08-29 17:14:52 -0700

    Rooms Spawn Enemies

--- MathMagics/Assets/Scripts/MapGeneration/RoomPreset.cs
+++ MathMagics/Assets/Scripts/MapGeneration/RoomPreset.cs
@@ -7,11 +7,28 @@ public class RoomPreset : MonoBehaviour
     public int roomWidth;
     public int roomHeight;
     public List<GameObject> enemies = new List<GameObject>();
-    public int numEnemies;
+    public List<Vector2> enemySpawnPositions = new();
+    public GameObject enemyPrefab;
+    public int maxNumEnemies = 2;
 
 
+    private void Start()
+    {
+        SpawnEnemies();       
+    }
+    
     public void SpawnEnemies()
     {
-        
+        if (enemyPrefab == null || maxNumEnemies == 0 || enemySpawnPositions.Count == 0) return;
+        List<Vector2> tempList = enemySpawnPositions;
+        int numSpawns = Random.Range(1, maxNumEnemies + 1);
+        for (int i = 0; i < numSpawns; i++)
+        {
+            //Spawn an enemy
+            Vector2 randomLocation = tempList[Random.Range(0, tempList.Count)];
+
+            Instantiate(enemyPrefab, randomLocation, Quaternion.identity, transform);
+            tempList.Remove(randomLocation);
+        }
     }
 }
